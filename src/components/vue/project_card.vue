7c1fb0d8ab3974da451845be007aacf6fc057d8e
Aren Marsden <aren.marsden6@gmail.com>
2024-08-15 21:27:48 +0100

    Add fade animation
    Signed-off-by: Aren Marsden <aren.marsden6@gmail.com>

--- src/components/vue/project_card.vue
+++ src/components/vue/project_card.vue
@@ -1,34 +1,37 @@
 <template>
-    <a :href="project.url" class="block bg-gray-700 shadow-lg rounded-lg overflow-hidden border border-gray-900 hover:bg-gray-800">
-        <!-- <img class="w-full h-48 object-cover" :src="project.image" alt="Project Image"> !-->
-        <div class="p-4">
-            <h3 class="text-xl font-semibold text-white">{{ project.title }}</h3>
-            <p class="text-white">{{ project.description }}</p>
-            <div class="flex items-center mt-4">
-                <span class="text-gray-700 mr-2 text-white">Language:</span>
-                <span class="text-gray-600 text-white">{{ project.language }}</span>
-            </div>
-            <div class="flex items-center mt-2">
-                <span class="text-gray-700 mr-2 text-white">Stars:</span>
-                <span class="text-gray-600 text-white">{{ project.stars }}</span>
-            </div>
-            <div class="flex items-center mt-2">
-                <span class="text-gray-700 mr-2 text-white">Last Updated:</span>
-                <span class="text-gray-600 text-white">{{ project.lastUpdated }}</span>
-            </div>
-        </div>
-    </a>
+	<a
+		:href="project.url"
+		class="animate-fade block overflow-hidden rounded-lg border border-gray-900 bg-gray-700 shadow-lg hover:bg-gray-800"
+	>
+		<!-- <img class="w-full h-48 object-cover" :src="project.image" alt="Project Image"> !-->
+		<div class="p-4">
+			<h3 class="text-xl font-semibold text-white">{{ project.title }}</h3>
+			<p class="text-white">{{ project.description }}</p>
+			<div class="mt-4 flex items-center">
+				<span class="mr-2 font-semibold text-gray-700 text-white">Language:</span>
+				<span class="text-gray-600 text-white">{{ project.language }}</span>
+			</div>
+			<div class="mt-2 flex items-center">
+				<span class="mr-2 font-semibold text-gray-700 text-white">Stars:</span>
+				<span class="text-gray-600 text-white">{{ project.stars }}</span>
+			</div>
+			<div class="mt-2 flex items-center">
+				<span class="mr-2 font-semibold text-gray-700 text-white">Last Updated:</span>
+				<span class="text-gray-600 text-white">{{ project.lastUpdated }}</span>
+			</div>
+		</div>
+	</a>
 </template>
 
 <script>
 export default {
-    name: 'ProjectCard',
-    props: {
-        project: {
-            type: Object,
-            required: true
-        }
-    }
+	name: 'ProjectCard',
+	props: {
+		project: {
+			type: Object,
+			required: true
+		}
+	}
 }
 </script>
 
